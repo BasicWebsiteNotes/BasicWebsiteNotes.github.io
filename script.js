49748ae95e62034395eacd709db290aea7a44205
BasicWebsiteNotes <94315709+BasicWebsiteNotes@users.noreply.github.com>
2021-11-19 00:31:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 var editBtn = document.getElementById('editBtn');
-var editables = document.querySelectorAll('#title, #author, #content');
-
-if (typeof(Storage) !== "undefined") {
-  if (localStorage.getItem('title') !== null) {
-    editables[0].innerHTML = localStorage.getItem('title');
-  }
-  if (localStorage.getItem('author') !== null) {
-    editables[1].innerHTML = localStorage.getItem('author');
-  }
-  if (localStorage.getItem('content') !== null) {
-    editables[2].innerHTML = localStorage.getItem('content');
-  }
-}
+var editables = document.querySelectorAll('#title, #author, #content')
 
 editBtn.addEventListener('click', function(e) {
   if (!editables[0].isContentEditable) {
@@ -34,9 +22,3 @@ editBtn.addEventListener('click', function(e) {
     }
   }
 });
-
-setInterval(function() {
-  for (var i = 0; i < editables.length; i++) {
-    localStorage.setItem(editables[i].getAttribute('id'), editables[i].innerHTML);
-  }
-}, 5000);
